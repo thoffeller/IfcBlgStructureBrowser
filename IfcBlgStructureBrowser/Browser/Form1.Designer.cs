c0a7b9802b32d3510506344c8650eab96018ae6b
thoffeller <th@cad-development.de>
2018-04-20 16:40:12 +0200

    first version with sample read / write

---
+++ IfcBlgStructureBrowser/Browser/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._buttonOpen = new System.Windows.Forms.Button();
+            this._richTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _buttonOpen
+            // 
+            this._buttonOpen.Location = new System.Drawing.Point(12, 12);
+            this._buttonOpen.Name = "_buttonOpen";
+            this._buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this._buttonOpen.TabIndex = 0;
+            this._buttonOpen.Text = "Open IfcXML ...";
+            this._buttonOpen.UseVisualStyleBackColor = true;
+            this._buttonOpen.Click += new System.EventHandler(this._buttonOpen_Click);
+            // 
+            // _richTextBox
+            // 
+            this._richTextBox.Location = new System.Drawing.Point(103, 12);
+            this._richTextBox.Name = "_richTextBox";
+            this._richTextBox.Size = new System.Drawing.Size(386, 363);
+            this._richTextBox.TabIndex = 1;
+            this._richTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 387);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this._richTextBox);
+            this.Controls.Add(this._buttonOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _buttonOpen;
+        private System.Windows.Forms.RichTextBox _richTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IfcBlgStructureBrowser/Browser/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    public partial class Form1 : Form
+    {
+        static string m_filename = "";
+        static Ifc4.Document m_ifcDocument = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "ifcXml files (*.ifcxml)|*.ifcxml";
+            dlg.CheckFileExists = true;
+
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                m_filename = dlg.FileName;
+            }
+
+            LoadXML();
+        }
+
+        private void LoadXML()
+        {
+            if (File.Exists(m_filename))
+            {
+                m_ifcDocument = Ifc4.Workspace.CurrentWorkspace.OpenDocument(m_filename);
+                if(m_ifcDocument != null)
+                {
+                    try
+                    {
+                        _richTextBox.Text += m_ifcDocument.Project.Name + "\n";
+                        if (m_ifcDocument.Project.Sites != null)
+                        {
+                            Ifc4.IfcSite site = m_ifcDocument.Project.Sites.First();
+
+                            _richTextBox.Text += site.LongName + "\n";
+                            foreach (Ifc4.IfcBuilding bldg in site.Buildings)
+                            {
+                                _richTextBox.Text += bldg.LongName + "\n";
+
+                                foreach (Ifc4.IfcBuildingStorey storey in bldg.BuildingStoreys)
+                                {
+                                    _richTextBox.Text += storey.LongName + "\n";
+
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ifcFullName = @"C:\tmp\a.ifcxml";
+            Ifc4.Document document = Ifc4.Workspace.CurrentWorkspace.CreateDocument(ifcFullName, true);
+
+            var header = document.IfcXmlDocument.Header;
+            header.Organization = "My Organisation";
+
+            var ifcSite = document.Project.Sites.AddNewSite();
+            ifcSite.LongName = "A-Project";
+
+            var ifcBuilding = ifcSite.Buildings.AddNewBuilding();
+            ifcBuilding.LongName = "B-Building";
+
+            var ifcBuildingStorey = ifcBuilding.BuildingStoreys.AddNewBuildingStorey();
+            ifcBuildingStorey.LongName = "C-Storey";
+
+            var ifcSpace = ifcBuildingStorey.Spaces.AddNewSpace();
+            ifcSpace.LongName = "D-Room";
+
+            document.Workspace.SaveDocument(ifcFullName);
+        }
+    }
+}
